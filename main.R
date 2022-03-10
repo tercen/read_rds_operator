ea4ff93c8227e677e71dafb57022d88a67aa0e4e
lucaslelann <lucas.lelann@gmail.com>
2022-03-10 11:11:51 +0000

    ci(tests): add github workflows and setup tests folder

--- main.R
+++ main.R
@@ -1,12 +1,6 @@
 library(tercen)
 library(dplyr)
 
-# http://localhost:53322/index.html#/alex/w/e11cea9d1119c8e6dbb0842925006475/ds/cb1e42fd-a9cb-459a-ab2f-86b297fa7163
-# options("tercen.serviceUri"="http://172.17.0.1:5400/api/v1/")
-# options("tercen.workflowId"= "e11cea9d1119c8e6dbb0842925006475")
-# options("tercen.stepId"= "cb1e42fd-a9cb-459a-ab2f-86b297fa7163")
-
-
 is.POSIXct <- function(x) inherits(x, "POSIXct")
 
 doc_to_data = function(df){
@@ -21,12 +15,12 @@ doc_to_data = function(df){
     mutate(.ci= rep_len(df$.ci[1], nrow(.)))
 }
 
-ctx = tercenCtx()
+ctx <- tercenCtx()
 
 if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
 
 ctx$cselect() %>% 
-  mutate(.ci= 1:nrow(.)-1) %>%
+  mutate(.ci= 1:nrow(.)-1L) %>%
   split(.$.ci) %>%
   lapply(doc_to_data) %>%
   bind_rows() %>%
--- workspace.R
+++ workspace.R
@@ -7,9 +7,34 @@ options("tercen.stepId"     = "dddd")
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+
+tabl<-read.csv2(file = "./tests/c03_simple_exp.tsv", sep="\t")
+
+saveRDS(tabl, file = "./tests/c03_simple_exp.rds", ascii = FALSE, version = NULL,compress = TRUE, refhook = NULL)
+
+
+is.POSIXct <- function(x) inherits(x, "POSIXct")
+
+doc_to_data = function(df){
+  filename = tempfile()
+  writeBin(ctx$client$fileService$download(df$documentId[1]), filename)
+  on.exit(unlink(filename))
+  readRDS(filename) %>% 
+    as_tibble() %>%
+    mutate_if(is.POSIXct, as.character) %>%
+    mutate_if(is.logical, as.character) %>%
+    mutate_if(is.integer, as.double) %>%
+    mutate(.ci= rep_len(df$.ci[1], nrow(.)))
+}
+
+ctx <- tercenCtx()
+
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+
+ctx$cselect() %>% 
+  mutate(.ci= 1:nrow(.)-1L) %>%
+  split(.$.ci) %>%
+  lapply(doc_to_data) %>%
+  bind_rows() %>%
   ctx$addNamespace() %>%
   ctx$save()
